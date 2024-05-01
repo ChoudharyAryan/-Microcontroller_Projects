40eed1f8288d6ad74560fa0a9b78c47b66073cf4
Aryan Choudhary <118959906+ChoudharyAryan@users.noreply.github.com>
2024-05-01 20:35:35 +0530

    Update main.cpp
    code for ESP8266_GSM_switch

--- code_for_esp8266_and_gsm_module_to_send,_receive_messages_and_calls_and_trip_the_realy_accordingly/src/main.cpp
+++ code_for_esp8266_and_gsm_module_to_send,_receive_messages_and_calls_and_trip_the_realy_accordingly/src/main.cpp
@@ -1,15 +1,22 @@
 #include <SoftwareSerial.h>
+//#include<EEPROM.h>
 const int LED = 5;
 int st = 0;
+const int h =1;
+const int l =0;
 long long int number = 0;
 long long int saveNumber = 0;
+int isDataValid;
 bool toogle = false;
 String phoneNumber;
 String message;
+//int FLAG_ADDRESS = 0;
 //Create software serial object to communicate with SIM800L
 SoftwareSerial mySerial(4, 2);  //SIM800L Tx & Rx is connected to Arduino #3 & #2
 
 void setup() {
+
+  delay(20000);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
   //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
@@ -17,9 +24,11 @@ void setup() {
 
   //Begin serial communication with Arduino and SIM800L
   mySerial.begin(9600);
+  //EEPROM.begin(sizeof(int)); // Initialize EEPROM
 
   Serial.println("Initializing...");
   delay(1000);
+  
 
   mySerial.println("AT");  //Once the handshake test is successful, it will back to OK
 
@@ -28,8 +37,8 @@ void setup() {
 
   mySerial.println("AT+CNMI=1,2,0,0,0");  // Decides how newly arrived SMS messages should be handled
   mySerial.println("AT+DDET=1");
-  mySerial.println("AT+CSQ");
-  updateSerial();
+  mySerial.println("AT+CSQ"); 
+  
 }
 
 void loop() {
@@ -37,6 +46,13 @@ void loop() {
 }
 
 void updateSerial() {
+  // EEPROM.get(FLAG_ADDRESS, isDataValid); // Read the flag from EEPROM
+  // Serial.print(isDataValid);
+  // delay(500);
+  // if(isDataValid){
+  //   delay(100);
+  // digitalWrite(LED, HIGH);
+  // }
   while (mySerial.available()) {
 
     message = mySerial.readStringUntil('\n');
@@ -70,6 +86,10 @@ void updateSerial() {
       mySerial.write(26);
       Serial.println("MESSAGESENT on");
       number = 0;
+      //EEPROM.put(FLAG_ADDRESS,h);
+      //Serial.println("COMMIT");
+      //EEPROM.commit();
+      //delay(500);
 
     } else if (message.startsWith("Off")) {
       digitalWrite(LED, LOW);
@@ -83,10 +103,15 @@ void updateSerial() {
       mySerial.write(26);
       Serial.println("MESSAGESENT of");
       number = 0;
+      //EEPROM.put(FLAG_ADDRESS,l);
+      //Serial.println("COMMIT");
+      //EEPROM.commit();
+      //delay(500);
 
     } else if (message.startsWith("R")) {
       mySerial.println("ATA");
       Serial.println("CALL is ANSWERED");
+      number=0;
       answerCall();
     } else if (message.startsWith("S")) {
       String phoneNumber = "+91" + String(number);  // Construct the phone number string
@@ -129,9 +154,9 @@ void answerCall() {
         ch = mySerial.readStringUntil('\n');
         Serial.println(ch);
       }
-      Serial.println(ch);
-      if (ch.indexOf('1') != -1) {
-        Serial.println("remothered");
+      //Serial.println(ch);
+     if(ch.startsWith("+DTMF:") or ch.startsWith("NO")){
+       if (ch.indexOf('1') != -1) {
         Serial.println(ch);
         Serial.println("ch is 1");
         digitalWrite(LED, HIGH);
@@ -145,6 +170,13 @@ void answerCall() {
         delay(100);
         mySerial.write(26);
         Serial.println("MESSAGESENT on");
+        number =0;
+        //EEPROM.put(FLAG_ADDRESS,h);
+        //Serial.println("COMMIT3");
+        //EEPROM.commit();
+        //delay(500);
+        //Serial.println(EEPROM.get(FLAG_ADDRESS,isDataValid));
+        //Serial.println(isDataValid);
         //mySerial.println("AT+CLVL=100");  // Set volume level
         //delay(1000);
         //mySerial.println("AT+TTS=0,\"Switch on\"");  // Play TTS for "Switch off"
@@ -153,7 +185,8 @@ void answerCall() {
       } 
       else if (ch.indexOf('0') != -1) {
         Serial.println("ch is 0");        
-        
+        digitalWrite(LED, LOW);
+        toogle = false;
         delay(1000);
         mySerial.print("AT+CMGS=\"");
         mySerial.print(phoneNumber);
@@ -163,19 +196,29 @@ void answerCall() {
         delay(100);
         mySerial.write(26);
         Serial.println("MESSAGESENT off");
-        delay(2000);
+        number =0;
+        //EEPROM.put(FLAG_ADDRESS,l);
+        //Serial.println("COMMIT4");
+        //EEPROM.commit();
+        //delay(500);
+        //Serial.println(EEPROM.get(FLAG_ADDRESS,isDataValid));
+        //Serial.println(isDataValid);
+        //delay(2000);
         // mySerial.println("AT+CLVL=100");  // Set volume level
         //delay(1000);
         //mySerial.println("AT+TTS=0,\"Switch off\"");  // Play TTS for "Switch off"
         //delay(5000);                                  // Wait for the message to be played
-        digitalWrite(LED, LOW);
-        toogle = false;
+        
+        
       } else if (ch.indexOf("NO") != -1) {
         Serial.println("call is ended");
+        number =0;
         break;
       }
+     }
     }
   }
   Serial.println("out of the loop");
+  number =0;
   updateSerial();
 }
